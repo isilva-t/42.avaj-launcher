1d2be34869541078c2b1ab7e9c4a03234906300a
isilva-t <ivanrfteixeira@gmail.com>
2026-01-21 15:01:50 +0000

    WeatherProvider base

---
+++ project/src/pt/isilvat/avaj/weather/WeatherProvider.java
@@ -0,0 +1,21 @@
+package pt.isilvat.avaj.weather;
+
+import pt.isilvat.avaj.coordinates.Coordinates;
+
+public class WeatherProvider {
+
+    private static WeatherProvider instance = new WeatherProvider();
+    private String[] weather = {"RAIN", "FOG", "SUN", "SNOW"};
+
+    private WeatherProvider() {
+    }
+
+    public static WeatherProvider getInstance() {
+        return instance;
+    }
+
+    public String getCurrentWeather(Coordinates p_coordinates) {
+        // needs logic here to generate correct weather
+        return weather[0];
+    }
+}
