40ffad0739562f33e6c92d09fc6ed0a8326c5134
isilva-t <ivanrfteixeira@gmail.com>
2026-01-23 17:29:48 +0000

    Aircraft - constructor variable assignment

--- project/src/pt/isilvat/avaj/aircraft/Aircraft.java
+++ project/src/pt/isilvat/avaj/aircraft/Aircraft.java
@@ -7,8 +7,16 @@ public abstract class Aircraft extends Flyable {
 
     protected long id;
     protected String name;
+    protected Coordinates coordinates;
 
-    private Aircraft() {
+    protected Aircraft(long p_id,
+        String p_name, 
+        Coordinates p_coordinates) {
 
+        this.id = p_id;
+        this.name = p_name;
+        this.coordinates = p_coordinates;
     }
+
+
 }
