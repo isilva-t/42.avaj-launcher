a50d400452824b1ab85db19d60324c6b873820de
isilva-t <ivanrfteixeira@gmail.com>
2026-01-28 09:23:48 +0000

    feat: unregister(Flyable) implementation
    s

--- project/src/pt/isilvat/avaj/weather/Tower.java
+++ project/src/pt/isilvat/avaj/weather/Tower.java
@@ -11,15 +11,19 @@ public class Tower {
     }
 
     private List<Flyable> observers = new ArrayList<>();
-
+    private final String name = "Tower Says: ";
     protected void conditionChanged() {
 
     }
 
     public void register(Flyable p_flyable) {
-
+        this.observers.add(p_flyable);
     }
+    
     public void unregister(Flyable p_flyable) {
-
+        String flyableName = p_flyable.getFullName();
+        String className = this.getClass().getSimpleName();
+        System.out.println(this.name + flyableName + "unregistered from " + className);
+        this.observers.remove(p_flyable);
     }
 }
