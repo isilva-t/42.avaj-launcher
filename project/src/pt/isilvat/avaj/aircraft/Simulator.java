c57efe4ae9df6e2c3601172f5854a447452a97ed
isilva-t <ivanrfteixeira@gmail.com>
2026-01-28 09:36:41 +0000

    feat: added Flyables to WeatherTower on Simulator

--- project/src/pt/isilvat/avaj/aircraft/Simulator.java
+++ project/src/pt/isilvat/avaj/aircraft/Simulator.java
@@ -3,19 +3,32 @@ package pt.isilvat.avaj.aircraft;
 import pt.isilvat.avaj.factory.AircraftFactory;
 import pt.isilvat.avaj.weather.WeatherTower;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Simulator {
 
-    private static WeatherTower weatherTower;
+    private static WeatherTower weatherTower = null;
 
     public static void main(String[] args) {
 
         weatherTower = new WeatherTower();
         AircraftFactory aircraftFactory = AircraftFactory.getInstance();
 
+        List<Flyable> flyables = new ArrayList<>();
+
+
         Coordinates coords = new Coordinates(10, 10, 10);
         Flyable balloon = aircraftFactory.newAircraft("Balloon","B1", coords);            
         Flyable helicopter = aircraftFactory.newAircraft("Helicopter","H1", coords);            
         Flyable jetplane = aircraftFactory.newAircraft("JetPlane","B1", coords);
+        flyables.add(balloon);
+        flyables.add(helicopter);
+        flyables.add(jetplane);
+        for(Flyable flyable: flyables) {
+            flyable.registerTower(weatherTower);
+        }
+
 
         int simulationRunningTimes = 3;
         for (; simulationRunningTimes > 0; simulationRunningTimes--) {
