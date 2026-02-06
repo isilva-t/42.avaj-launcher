8c07da8bcf7ee3b3e59925b30820486bcdefb0da
isilva-t <ivanrfteixeira@gmail.com>
2026-02-06 06:57:49 +0000

    feat: Simulation - start reading file.txt

--- project/src/pt/isilvat/avaj/aircraft/Simulator.java
+++ project/src/pt/isilvat/avaj/aircraft/Simulator.java
@@ -7,34 +7,70 @@ import pt.isilvat.avaj.printer.Printer;
 import java.util.List;
 import java.util.ArrayList;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+
 public class Simulator {
 
     private static WeatherTower weatherTower = null;
 
+    private static int simulationCycles;
+
+    private static void loadData(String fileName) {
+        try {
+            File file = new File(fileName);
+            BufferedReader br = new BufferedReader(
+                new FileReader(file));
+
+            String line;
+
+            while((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (Exception e) {
+
+        }
+    }
+
+
+
     public static void main(String[] args) {
-
-        weatherTower = new WeatherTower();
-
-        List<Flyable> flyables = new ArrayList<>();
-
-        Printer.print("", "");
-
-        Coordinates coords = new Coordinates(10, 10, 10);
-        Flyable balloon = AircraftFactory.newAircraft("Balloon","B1", coords);            
-        Flyable helicopter = AircraftFactory.newAircraft("Helicopter","H1", coords);            
-        Flyable jetplane = AircraftFactory.newAircraft("JetPlane","B1", coords);
-        flyables.add(balloon);
-        flyables.add(helicopter);
-        flyables.add(jetplane);
-        for(Flyable flyable: flyables) {
-            flyable.registerTower(weatherTower);
+        if (args.length < 1) {
+            return;
         }
 
+        try {
+            loadData(args[0]);
 
-        int simulationRunningTimes = 15;
-        for (; simulationRunningTimes > 0; simulationRunningTimes--) {
-            weatherTower.changeWeather();
+            weatherTower = new WeatherTower();
+
+            List<Flyable> flyables = new ArrayList<>();
+
             Printer.print("", "");
+
+            Coordinates coords = new Coordinates(10, 10, 10);
+            Flyable balloon = AircraftFactory.newAircraft("Balloon","B1", coords);            
+            Flyable helicopter = AircraftFactory.newAircraft("Helicopter","H1", coords);            
+            Flyable jetplane = AircraftFactory.newAircraft("JetPlane","B1", coords);
+            flyables.add(balloon);
+            flyables.add(helicopter);
+            flyables.add(jetplane);
+            for(Flyable flyable: flyables) {
+                flyable.registerTower(weatherTower);
+            }
+
+
+            int simulationRunningTimes = 15;
+            for (; simulationRunningTimes > 0; simulationRunningTimes--) {
+                weatherTower.changeWeather();
+                Printer.print("", "");
+            }
+        } catch (Exception e) {
+            System.out.println("oops");
         }
     }
 }
