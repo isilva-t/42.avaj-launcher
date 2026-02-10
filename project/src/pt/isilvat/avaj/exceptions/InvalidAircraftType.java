4d9085b67afe2c99f8ba82273d853b67fb35d429
isilva-t <ivanrfteixeira@gmail.com>
2026-02-10 14:49:19 +0000

    feat: InvalidAircraftType exception

---
+++ project/src/pt/isilvat/avaj/exceptions/InvalidAircraftType.java
@@ -0,0 +1,12 @@
+package pt.isilvat.avaj.exceptions;
+
+public class InvalidAircraftType extends Exception {
+
+    public InvalidAircraftType() {
+        super("Invalid Aircraft Type!");
+    }
+
+    public InvalidAircraftType(String invalidAircraft) {
+        super("Invalid Aircraft Type" + invalidAircraft);
+    }
+}
