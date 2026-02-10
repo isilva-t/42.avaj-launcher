50f1333c675ba71e1583740e20679540bcec831d
isilva-t <ivanrfteixeira@gmail.com>
2026-02-10 14:33:42 +0000

    feat: InvalidSimulationCycles exception

---
+++ project/src/pt/isilvat/avaj/exceptions/InvalidSimulationCycles.java
@@ -0,0 +1,8 @@
+package pt.isilvat.avaj.exceptions;
+
+public class InvalidSimulationCycles extends Exception {
+
+    public InvalidSimulationCycles() {
+        super("Invalid simulationCycles. Please check it!");
+    }
+}
